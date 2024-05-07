74980b8eedd5e38f957f4a9481b50e769c5cc00e
Kevin Ocampo <161964924+Ocampo81@users.noreply.github.com>
2024-05-07 16:22:03 +0000

    Kevin Commit

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="navbar bg-dark border-bottom border-body text-white p-3">
+            <p className="m-auto">Copyright © your website 2024</p>
+        </div>
+    )
+}
+
+export {Footer};
